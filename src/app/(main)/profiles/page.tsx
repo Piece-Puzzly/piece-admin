45cbecd2f35b12cb2c8898a7d037a6a9da013849
hyomin <miamiq0000@gmail.com>
2025-04-07 14:34:34 +0900

    x

--- src/app/(main)/profiles/page.tsx
+++ src/app/(main)/profiles/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({
   searchParams: Promise<{ page?: string }>;
 }) {
   const params = await searchParams;
-  const data = await getProfiles(parseInt(params.page) - 1 || 0);
+  const data = await getProfiles(params.page ? parseInt(params.page) - 1 : 0);
   return (
     <div className="space-y-[44px] mb-[86px]">
       <DataTable columns={columns} data={data.content} />
