3f2275a918bf05f5690613fd5c0541aa4c50252e
hyomin <miamiq0000@gmail.com>
2025-04-27 06:36:05 +0900

    로고 화질 개선

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
         <Image
           src={"/logo.png"}
           height={80}
-          width={80}
+          width={214}
           className="w-auto h-[80px]"
           alt="Piece"
         />
