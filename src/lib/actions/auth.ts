b43c60df83b34f669c513dbc6035fb45f0727940
hyomin <miamiq0000@gmail.com>
2025-08-05 04:09:48 +0900

    check auth

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -2,9 +2,26 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../auth-options";
 
-export async function checkAuth() {
+export async function checkAuth(): Promise<boolean> {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/login");
+  }
+
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_NEXTAUTH_BASE_URL + `/users?page=${1}&size=${1}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+      cache: "no-store",
+    }
+  );
+
+  if (!response.ok) {
+    redirect("/login");
+  } else {
+    return false;
   }
 }
