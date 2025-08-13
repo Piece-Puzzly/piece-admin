59a3999f6ba9ef823207c7608f05e3bc2d13c9e0
hyomin <miamiq0000@gmail.com>
2025-08-13 21:18:15 +0900

    x

--- src/app/(main)/user-list/role-none/page.tsx
+++ src/app/(main)/user-list/role-none/page.tsx
@@ -1,14 +1,16 @@
+import { checkAuth } from "@/lib/actions/auth";
 import prisma from "@/lib/prisma";
 import UserTableWithPagination from "../_components/user-table-with-pagination";
 
-const USERS_PER_PAGE = 10;
+const USERS_PER_PAGE = 20;
 
 export default async function UserPage({
   searchParams,
 }: {
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const currentPage = Number(searchParams?.page) || 1;
+  await checkAuth();
+  const currentPage = Number((await searchParams)?.page) || 1;
   const whereClause = { role: "NONE" };
 
   const [users, totalUsers] = await prisma.$transaction([
--- src/app/(main)/user-list/role-pending/page.tsx
+++ src/app/(main)/user-list/role-pending/page.tsx
@@ -1,20 +1,21 @@
 import prisma from "@/lib/prisma";
 import UserTableWithPagination from "../_components/user-table-with-pagination";
+import { checkAuth } from "@/lib/actions/auth";
 
-const USERS_PER_PAGE = 10;
+const USERS_PER_PAGE = 20;
 
 export default async function UserPage({
   searchParams,
 }: {
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const currentPage = Number(searchParams?.page) || 1;
+  await checkAuth();
+  const currentPage = Number((await searchParams)?.page) || 1;
   const whereClause = { role: "PENDING" };
 
   const [users, totalUsers] = await prisma.$transaction([
     prisma.user_table.findMany({
       where: whereClause,
-      include: { profile: true },
       skip: (currentPage - 1) * USERS_PER_PAGE,
       take: USERS_PER_PAGE,
       orderBy: { created_at: "desc" },
@@ -28,7 +29,7 @@ export default async function UserPage({
       totalCount={totalUsers}
       currentPage={currentPage}
       perPage={USERS_PER_PAGE}
-      roleName="정상 사용자"
+      roleName="승인 대기 사용자"
     />
   );
 }
