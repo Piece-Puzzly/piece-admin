9b695024d08a650436904710722f37d28cb12c7c
hyomin <miamiq0000@gmail.com>
2025-08-28 13:23:26 +0900

    매칭 삭제 클릭 시 dialog로 확인하고 삭제

--- src/app/(main)/match-action/_components/delete-match-button.tsx
+++ src/app/(main)/match-action/_components/delete-match-button.tsx
@@ -1,4 +1,14 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { deleteMatchInfo } from "@/lib/actions/match-infos";
 import { useState } from "react";
 
@@ -9,20 +19,40 @@ export default function DeleteMatchButton({
 }) {
   const [loading, setLoading] = useState(false);
   return (
-    <Button
-      className="h-full w-full text-destructive hover:text-destructive"
-      variant={"ghost"}
-      onClick={async () => {
-        setLoading(true);
-        try {
-          await deleteMatchInfo(matchId);
-        } finally {
-          setLoading(false);
-        }
-      }}
-      disabled={loading}
-    >
-      {loading ? "삭제 중..." : "삭제"}
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          className="h-full w-full text-destructive hover:text-destructive"
+          variant={"ghost"}
+        >
+          {"삭제"}
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>정말로 삭제하시겠습니까?</DialogTitle>
+          <DialogDescription />
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="ghost">취소</Button>
+          </DialogClose>
+          <Button
+            variant={"destructive"}
+            onClick={async () => {
+              setLoading(true);
+              try {
+                await deleteMatchInfo(matchId);
+              } finally {
+                setLoading(false);
+              }
+            }}
+            disabled={loading}
+          >
+            {loading ? "삭제 중..." : "삭제"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
